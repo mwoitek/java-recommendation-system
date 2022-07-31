1629d921694c7a5fd2e040defd6165e69b2e15ff
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-31 14:30:41 -0300

    rewrite code so that test methods look similar

--- src/main/FirstRatings.java
+++ src/main/FirstRatings.java
@@ -71,66 +71,54 @@ public class FirstRatings {
     return directorCountMap;
   }
 
-  private Integer getMaximumNumberMovies(HashMap<String, Integer> directorCountMap) {
-    Integer maxNumMovies = -1;
-    Integer numMovies;
-    for (String director : directorCountMap.keySet()) {
-      numMovies = directorCountMap.get(director);
-      if (numMovies > maxNumMovies) {
-        maxNumMovies = numMovies;
+  private Integer getMaximumValueInMap(HashMap<String, Integer> myMap) {
+    Integer maxValue = -1;
+    Integer value;
+    for (String key : myMap.keySet()) {
+      if ((value = myMap.get(key)) > maxValue) {
+        maxValue = value;
       }
     }
-    return maxNumMovies;
+    return maxValue;
   }
 
-  private ArrayList<String> getDirectorsMaximumNumberMovies(
-      HashMap<String, Integer> directorCountMap, Integer maxNumMovies) {
-    ArrayList<String> directors = new ArrayList<String>();
-    for (String director : directorCountMap.keySet()) {
-      if (directorCountMap.get(director) == maxNumMovies) {
-        directors.add(director);
+  private ArrayList<String> getKeysWithMaxValue(HashMap<String, Integer> myMap,
+      Integer maxValue) {
+    ArrayList<String> keys = new ArrayList<String>();
+    for (String key : myMap.keySet()) {
+      if (myMap.get(key) == maxValue) {
+        keys.add(key);
       }
     }
-    return directors;
+    return keys;
   }
 
-  private void printDirectorsInfo(ArrayList<Movie> movies) {
+  public void testLoadMovies() {
+    String filename = "ratedmoviesfull.csv";
+    ArrayList<Movie> movies = loadMovies(filename);
     HashMap<String, Integer> directorCountMap = buildDirectorCountMap(movies);
-    Integer maxNumMovies = getMaximumNumberMovies(directorCountMap);
+    Integer maxNumMovies = getMaximumValueInMap(directorCountMap);
+    ArrayList<String> directors = getKeysWithMaxValue(directorCountMap, maxNumMovies);
+
+    System.out.println("filename = " + filename);
+    System.out.println("Number of movies: " + movies.size());
+
+    // System.out.println("Movies:");
+    // for (Movie movie : movies) {
+    // System.out.println(movie);
+    // }
+
+    System.out
+        .println("Movies that have 'Comedy' as a genre: " + countComedyMovies(movies));
+    System.out.println("Movies that are longer than 150 minutes: "
+        + countMoviesLongerThan150Min(movies));
+
     System.out.println("Maximum number of movies by any director: " + maxNumMovies);
-    ArrayList<String> directors =
-        getDirectorsMaximumNumberMovies(directorCountMap, maxNumMovies);
     System.out.println("Directors that directed that many movies:");
     for (String director : directors) {
       System.out.println(director);
     }
-  }
-
-  public void testLoadMovies() {
-    String filename = "ratedmovies_short.csv";
-    ArrayList<Movie> movies = loadMovies(filename);
-    System.out.println("filename = " + filename);
-    System.out.println("Number of movies: " + movies.size());
-    System.out.println("Movies:");
-    for (Movie movie : movies) {
-      System.out.println(movie);
-    }
-    System.out
-        .println("Movies that have 'Comedy' as a genre: " + countComedyMovies(movies));
-    System.out.println("Movies that are longer than 150 minutes: "
-        + countMoviesLongerThan150Min(movies));
-    printDirectorsInfo(movies);
-    System.out.println();
-
-    filename = "ratedmoviesfull.csv";
-    movies = loadMovies(filename);
-    System.out.println("filename = " + filename);
-    System.out.println("Number of movies: " + movies.size());
-    System.out
-        .println("Movies that have 'Comedy' as a genre: " + countComedyMovies(movies));
-    System.out.println("Movies that are longer than 150 minutes: "
-        + countMoviesLongerThan150Min(movies));
-    printDirectorsInfo(movies);
+    System.out.println("Number of such directors: " + directors.size());
   }
 
   public ArrayList<Rater> loadRaters(String filename) {
@@ -161,26 +149,45 @@ public class FirstRatings {
     return raters;
   }
 
+  private HashMap<String, Integer> buildRaterNumRatingsMap(ArrayList<Rater> raters) {
+    HashMap<String, Integer> raterNumRatingsMap = new HashMap<String, Integer>();
+    for (Rater rater : raters) {
+      raterNumRatingsMap.put(rater.getID(), rater.numRatings());
+    }
+    return raterNumRatingsMap;
+  }
+
   public void testLoadRaters() {
     String filename = "ratings_short.csv";
     ArrayList<Rater> raters = loadRaters(filename);
-    System.out.println("filename = " + filename);
-    System.out.println("Number of raters: " + raters.size());
-    System.out.println("Raters:");
-    for (Rater rater : raters) {
-      System.out.println("ID: " + rater.getID() + ", # ratings: " + rater.numRatings());
-    }
-    System.out.println();
+    HashMap<String, Integer> raterNumRatingsMap = buildRaterNumRatingsMap(raters);
+    Integer maxNumRatings = getMaximumValueInMap(raterNumRatingsMap);
+    ArrayList<String> raterIds = getKeysWithMaxValue(raterNumRatingsMap, maxNumRatings);
 
-    filename = "ratings.csv";
-    raters = loadRaters(filename);
     System.out.println("filename = " + filename);
     System.out.println("Number of raters: " + raters.size());
+
+    // System.out.println("Raters:");
+    // for (String id : raterNumRatingsMap.keySet()) {
+    // System.out.println("ID: " + id + ", # ratings: " + raterNumRatingsMap.get(id));
+    // }
+
+    String raterId = "2";
+    System.out.println("# ratings for rater with ID " + raterId + ": "
+        + raterNumRatingsMap.get(raterId));
+
+    System.out.println("Maximum number of ratings by any rater: " + maxNumRatings);
+    System.out.println("Raters that have the maximum number of ratings:");
+    for (String id : raterIds) {
+      System.out.println(id);
+    }
+    System.out.println("Number of such raters: " + raterIds.size());
   }
 
   public static void main(String[] args) {
     FirstRatings fr = new FirstRatings();
-    // fr.testLoadMovies();
+    fr.testLoadMovies();
+    System.out.println();
     fr.testLoadRaters();
   }
 
