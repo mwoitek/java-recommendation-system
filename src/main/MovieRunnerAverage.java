fadc59c58ad331fec1bf817bd044abb8eff0a6e4
Marcio Woitek <mwoitekjr@gmail.com>
2022-08-01 13:21:51 -0300

    Completed week 2 assignment

--- src/main/MovieRunnerAverage.java
+++ src/main/MovieRunnerAverage.java
@@ -1,9 +1,52 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MovieRunnerAverage {
 
+  public void printAverageRatings() {
+    SecondRatings sr = new SecondRatings("ratedmovies_short.csv", "ratings_short.csv");
+    System.out.println("Number of movies: " + sr.getMovieSize());
+    System.out.println("Number of raters: " + sr.getRaterSize());
+
+    int minimalRaters = 3;
+    ArrayList<Rating> averageRatings = sr.getAverageRatings(minimalRaters);
+    Collections.sort(averageRatings);
+
+    String movieTitle;
+    for (Rating averageRating : averageRatings) {
+      movieTitle = sr.getTitle(averageRating.getItem());
+      System.out.println(averageRating.getValue() + " " + movieTitle);
+    }
+  }
+
+  public void getAverageRatingOneMovie() {
+    SecondRatings sr = new SecondRatings("ratedmovies_short.csv", "ratings_short.csv");
+
+    String movieTitle = "The Godfather";
+    String movieId = sr.getID(movieTitle);
+
+    ArrayList<Rating> averageRatings = sr.getAverageRatings(1);
+    int i = 0;
+    while (i < averageRatings.size()
+        && !averageRatings.get(i).getItem().equals(movieId)) {
+      i++;
+    }
+
+    if (i == averageRatings.size()) {
+      System.out.println("NO SUCH TITLE");
+    } else {
+      System.out.println(
+          "Average rating for " + movieTitle + ": " + averageRatings.get(i).getValue());
+    }
+  }
+
   public static void main(String[] args) {
-    //
+    MovieRunnerAverage runner = new MovieRunnerAverage();
+    runner.printAverageRatings();
+    System.out.println();
+    runner.getAverageRatingOneMovie();
   }
 
 }
--- src/main/SecondRatings.java
+++ src/main/SecondRatings.java
@@ -25,4 +25,57 @@ public class SecondRatings {
     return myRaters.size();
   }
 
+  private double getAverageByID(String id, int minimalRaters) {
+    double sum = 0.0;
+    int count = 0;
+    double rating;
+    for (Rater rater : myRaters) {
+      if ((rating = rater.getRating(id)) < 0.0) {
+        continue;
+      }
+      sum += rating;
+      count++;
+    }
+    if (count >= minimalRaters) {
+      return sum / count;
+    }
+    return 0.0;
+  }
+
+  public ArrayList<Rating> getAverageRatings(int minimalRaters) {
+    ArrayList<Rating> averageRatings = new ArrayList<Rating>();
+    String movieId;
+    double averageRating;
+    for (Movie movie : myMovies) {
+      movieId = movie.getID();
+      averageRating = getAverageByID(movieId, minimalRaters);
+      if (averageRating > 0.0) {
+        averageRatings.add(new Rating(movieId, averageRating));
+      }
+    }
+    return averageRatings;
+  }
+
+  public String getTitle(String id) {
+    int i = 0;
+    while (i < myMovies.size() && !myMovies.get(i).getID().equals(id)) {
+      i++;
+    }
+    if (i == myMovies.size()) {
+      return "MOVIE ID NOT FOUND";
+    }
+    return myMovies.get(i).getTitle();
+  }
+
+  public String getID(String title) {
+    int i = 0;
+    while (i < myMovies.size() && !myMovies.get(i).getTitle().equals(title)) {
+      i++;
+    }
+    if (i == myMovies.size()) {
+      return "NO SUCH TITLE";
+    }
+    return myMovies.get(i).getID();
+  }
+
 }
