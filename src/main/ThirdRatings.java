6d9edd62aa78f1f565159594b21684aae3496e42
Marcio Woitek <mwoitekjr@gmail.com>
2022-08-01 22:27:58 -0300

    Add ThirdRatings class

---
+++ src/main/ThirdRatings.java
@@ -0,0 +1,81 @@
+package main;
+
+import java.util.ArrayList;
+
+public class ThirdRatings {
+
+  private ArrayList<Movie> myMovies;
+  private ArrayList<EfficientRater> myRaters;
+
+  public ThirdRatings(String moviefile, String ratingsfile) {
+    FirstRatings fr = new FirstRatings();
+    myMovies = fr.loadMovies(moviefile);
+    myRaters = fr.loadRaters(ratingsfile);
+  }
+
+  public ThirdRatings() {
+    this("data/ratedmoviesfull.csv", "data/ratings.csv");
+  }
+
+  public int getMovieSize() {
+    return myMovies.size();
+  }
+
+  public int getRaterSize() {
+    return myRaters.size();
+  }
+
+  private double getAverageByID(String id, int minimalRaters) {
+    double sum = 0.0;
+    int count = 0;
+    double rating;
+    for (EfficientRater rater : myRaters) {
+      if ((rating = rater.getRating(id)) < 0.0) {
+        continue;
+      }
+      sum += rating;
+      count++;
+    }
+    if (count >= minimalRaters) {
+      return sum / count;
+    }
+    return 0.0;
+  }
+
+  public ArrayList<Rating> getAverageRatings(int minimalRaters) {
+    ArrayList<Rating> averageRatings = new ArrayList<Rating>();
+    String movieId;
+    double averageRating;
+    for (Movie movie : myMovies) {
+      movieId = movie.getID();
+      averageRating = getAverageByID(movieId, minimalRaters);
+      if (averageRating > 0.0) {
+        averageRatings.add(new Rating(movieId, averageRating));
+      }
+    }
+    return averageRatings;
+  }
+
+  public String getTitle(String id) {
+    int i = 0;
+    while (i < myMovies.size() && !myMovies.get(i).getID().equals(id)) {
+      i++;
+    }
+    if (i == myMovies.size()) {
+      return "MOVIE ID NOT FOUND";
+    }
+    return myMovies.get(i).getTitle();
+  }
+
+  public String getID(String title) {
+    int i = 0;
+    while (i < myMovies.size() && !myMovies.get(i).getTitle().equals(title)) {
+      i++;
+    }
+    if (i == myMovies.size()) {
+      return "NO SUCH TITLE";
+    }
+    return myMovies.get(i).getID();
+  }
+
+}
