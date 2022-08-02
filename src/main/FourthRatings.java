80b3045bd03a6ba1a44ae4b5e21f7800e78f2533
Marcio Woitek <mwoitekjr@gmail.com>
2022-08-02 19:05:06 -0300

    week 4 setup

---
+++ src/main/FourthRatings.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.util.ArrayList;
+
+public class FourthRatings {
+
+  private ArrayList<EfficientRater> myRaters;
+
+  public FourthRatings(String ratingsfile) {
+    FirstRatings fr = new FirstRatings();
+    myRaters = fr.loadRaters(ratingsfile);
+  }
+
+  public FourthRatings() {
+    this("data/ratings.csv");
+  }
+
+  public int getRaterSize() {
+    return myRaters.size();
+  }
+
+  private double getAverageByID(String id, int minimalRaters) {
+    double sum = 0.0;
+    int count = 0;
+    double rating;
+    for (EfficientRater rater : myRaters) {
+      if ((rating = rater.getRating(id)) < 0.0) {
+        continue;
+      }
+      sum += rating;
+      count++;
+    }
+    if (count >= minimalRaters) {
+      return sum / count;
+    }
+    return 0.0;
+  }
+
+  public ArrayList<Rating> getAverageRatings(int minimalRaters) {
+    ArrayList<Rating> averageRatings = new ArrayList<Rating>();
+    ArrayList<String> movieIds = MovieDatabase.filterBy(new TrueFilter());
+    double averageRating;
+    for (String movieId : movieIds) {
+      if ((averageRating = getAverageByID(movieId, minimalRaters)) > 0.0) {
+        averageRatings.add(new Rating(movieId, averageRating));
+      }
+    }
+    return averageRatings;
+  }
+
+  public ArrayList<Rating> getAverageRatingsByFilter(int minimalRaters,
+      Filter filterCriteria) {
+    ArrayList<Rating> averageRatings = new ArrayList<Rating>();
+    ArrayList<String> movieIds = MovieDatabase.filterBy(filterCriteria);
+    double averageRating;
+    for (String movieId : movieIds) {
+      if ((averageRating = getAverageByID(movieId, minimalRaters)) > 0.0) {
+        averageRatings.add(new Rating(movieId, averageRating));
+      }
+    }
+    return averageRatings;
+  }
+
+}
