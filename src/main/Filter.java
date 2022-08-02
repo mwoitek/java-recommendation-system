e528c0d541989a79a63eaae473950cf0416bd96d
Marcio Woitek <mwoitekjr@gmail.com>
2022-08-01 21:18:01 -0300

    add starter files for week 3

---
+++ src/main/Filter.java
@@ -0,0 +1,7 @@
+package main;
+
+public interface Filter {
+
+  public boolean satisfies(String id);
+
+}
---
+++ src/main/TrueFilter.java
@@ -0,0 +1,10 @@
+package main;
+
+public class TrueFilter implements Filter {
+
+  @Override
+  public boolean satisfies(String id) {
+    return true;
+  }
+
+}
