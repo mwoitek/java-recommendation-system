bf8b2c4f8503a2383ed33ab52a9571cf0d2899b1
Marcio Woitek <mwoitekjr@gmail.com>
2022-08-04 13:07:02 -0300

    improve MovieRunnerSimilarRatings

--- src/main/MovieRunnerSimilarRatings.java
+++ src/main/MovieRunnerSimilarRatings.java
@@ -57,6 +57,25 @@ public class MovieRunnerSimilarRatings {
     }
   }
 
+  private void printListOfRatings(ArrayList<Rating> similarRatings, int numRows) {
+    Rating similarRating;
+    String col1;
+    String col2;
+    String col3;
+    if (numRows > similarRatings.size()) {
+      numRows = similarRatings.size();
+    }
+    System.out.println("Rank\tWeighted Average\tTitle\n");
+    for (int i = 0; i < numRows; i++) {
+      similarRating = similarRatings.get(i);
+      col1 = String.format("%-4s", i + 1);
+      col2 = String.format("%.4f", similarRating.getValue());
+      col2 = String.format("%-16s", col2);
+      col3 = MovieDatabase.getTitle(similarRating.getItem());
+      System.out.println(col1 + "\t" + col2 + "\t" + col3);
+    }
+  }
+
   public void printSimilarRatings() {
     FourthRatings fr = new FourthRatings();
     MovieDatabase.initialize("ratedmoviesfull.csv");
@@ -65,29 +84,20 @@ public class MovieRunnerSimilarRatings {
     String id = "65";
     int numSimilarRaters = 20;
     int minimalRaters = 5;
+
     ArrayList<Rating> similarRatings =
         fr.getSimilarRatings(id, numSimilarRaters, minimalRaters);
-
-    Rating similarRating;
-    for (int i = 0; i < 15; i++) {
-      try {
-        similarRating = similarRatings.get(i);
-      } catch (Exception e) {
-        break;
-      }
-      System.out.println((i + 1) + " " + similarRating.getValue() + " "
-          + MovieDatabase.getTitle(similarRating.getItem()));
-    }
+    printListOfRatings(similarRatings, 15);
   }
 
   public static void main(String[] args) {
     MovieRunnerSimilarRatings runner = new MovieRunnerSimilarRatings();
 
-    runner.printAverageRatings();
-    System.out.println();
+    // runner.printAverageRatings();
+    // System.out.println();
 
-    runner.printAverageRatingsByYearAfterAndGenre();
-    System.out.println();
+    // runner.printAverageRatingsByYearAfterAndGenre();
+    // System.out.println();
 
     runner.printSimilarRatings();
   }
