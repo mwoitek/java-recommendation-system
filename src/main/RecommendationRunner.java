b3f85e563de71c31263ff5b8da1ee1e5a9f00d5f
Marcio Woitek <mwoitekjr@gmail.com>
2022-08-04 19:21:49 -0300

    setup for the final assignment

---
+++ src/main/RecommendationRunner.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.ArrayList;
+
+public class RecommendationRunner implements Recommender {
+
+  public ArrayList<String> getItemsToRate() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void printRecommendationsFor(String webRaterID) {
+    // TODO Auto-generated method stub
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+  }
+
+}
