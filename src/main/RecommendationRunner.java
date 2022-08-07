f4b36bdb35eccdc38a2f3e4efee3f9284737acde
Marcio Woitek <mwoitekjr@gmail.com>
2022-08-07 19:37:11 -0300

    Implement getItemsToRate

--- src/main/RecommendationRunner.java
+++ src/main/RecommendationRunner.java
@@ -1,20 +1,57 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class RecommendationRunner implements Recommender {
 
   public ArrayList<String> getItemsToRate() {
-    // TODO Auto-generated method stub
-    return null;
+    int numMovies = 15;
+    int minimalRaters = 5;
+    int numTopMovies = 45;
+
+    MovieDatabase.initialize("ratedmoviesfull.csv");
+    RaterDatabase.initialize("ratings.csv");
+
+    FourthRatings fr = new FourthRatings();
+    ArrayList<Rating> averageRatings = fr.getAverageRatings(minimalRaters);
+    Collections.sort(averageRatings, Collections.reverseOrder());
+
+    if (numTopMovies > averageRatings.size()) {
+      numTopMovies = averageRatings.size();
+    }
+
+    ArrayList<String> itemsToRate = new ArrayList<>();
+    Random rng = new Random();
+    int randomIndex;
+    String movieId;
+
+    while (itemsToRate.size() < numMovies) {
+      randomIndex = rng.nextInt(numTopMovies);
+      movieId = averageRatings.get(randomIndex).getItem();
+      if (itemsToRate.contains(movieId)) {
+        continue;
+      }
+      itemsToRate.add(movieId);
+    }
+
+    return itemsToRate;
   }
 
   public void printRecommendationsFor(String webRaterID) {
-    // TODO Auto-generated method stub
+    //
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    RecommendationRunner runner = new RecommendationRunner();
+
+    ArrayList<String> itemsToRate = runner.getItemsToRate();
+    for (String movieId : itemsToRate) {
+      System.out.println(movieId);
+    }
+
+    //
   }
 
 }
